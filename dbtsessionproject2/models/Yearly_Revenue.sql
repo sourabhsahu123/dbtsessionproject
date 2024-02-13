fa15772ca6153d1570029b8df03125fac94cb088
Sourabh <souruitian@gmail.com>
2024-02-13 20:49:19 +0530

    Create YoY Sales Upated with month Model

--- dbtsessionproject2/models/Yearly_Revenue.sql
+++ dbtsessionproject2/models/Yearly_Revenue.sql
@@ -1,10 +1,11 @@
 WITH CTE_YoY_Sales AS (
 
     SELECT YEAR(INVOICEDATE) Y,
+     MONTH(INVOICEDATE) M,
     SUM(TOTAL) T
      from RAW_SALES
-    GROUP BY YEAR(INVOICEDATE)
+    GROUP BY YEAR(INVOICEDATE), MONTH(INVOICEDATE)
 )
-SELECT  "Y" as "Year",
+SELECT  "Y" as "Year", "M" as "Month",
 "T" as "Revenue"
 from CTE_YoY_Sales
